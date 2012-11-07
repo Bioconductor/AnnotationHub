9a3e8aafd4c8ed037d1c0f93eed9f94484725ab1
Marc Carlson <mcarlson@fhcrc.org>
2012-11-07 21:29:17 +0000

    updates to expose completion.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@71044 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AllClasses.R
+++ R/AllClasses.R
@@ -1,11 +1,3 @@
 setClass("AnnotationHub", representation(paths="character"))
 
 
-## define method here?
-.DollarNames.AnnotationHub <- function(x, pattern=""){
-  grep(pattern, x@paths, value=TRUE)
-}
-
-## and this (to define a method for $ (so we can actually return something)
-setMethod("$", "AnnotationHub", function(x, name){name})
-
--- R/methods-search.R
+++ R/methods-search.R
@@ -46,3 +46,11 @@
 ## setMethod("$", "AnnotationHub", function(x, name){name})
 
 
+## define method here?
+.DollarNames.AnnotationHub <- function(x, pattern=""){
+  grep(pattern, x@paths, value=TRUE)
+}
+
+## and this (to define a method for $ (so we can actually return something)
+setMethod("$", "AnnotationHub", function(x, name){name})
+
