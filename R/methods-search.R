357ffb9560b7cafbbcc2c5fcf65b3774563a90fa
Marc Carlson <mcarlson@fhcrc.org>
2012-11-14 01:15:29 +0000

    More cleanup removes unecessary helper.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@71190 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/methods-search.R
+++ R/methods-search.R
@@ -15,18 +15,6 @@
 }
   
 
-## take a name, some paths and a pattern and returns either the single value,
-## or just the set that matches.  Basically it's a grep, but IF there's a name,
-## then we have to have a special case and check for a single exact match.
-.getPotentialNames <- function(paths, pattern, name=NA){
-  if(!is.na(name) && name %in% paths){
-    return(name)
-  }else{
-    #return(.DollarNames.AnnotationHub(AnnotationHub, pattern) )
-    return(grep(pattern, paths, value=TRUE))
-  }
-}
-
 
 ## This is the workhorse for the tab stuff.
 ## When the user hits tab, we want to complete what we can here
@@ -39,8 +27,7 @@
   
   ## But in spite of all that stuff happening above,
   ## this is what we always want to return:
-  ## grep(x@pattern, paths, value=TRUE)
-  .getPotentialNames(paths=paths, pattern=x@pattern)
+  grep(x@pattern, paths, value=TRUE)
 }
 
 
@@ -51,7 +38,7 @@
 
 .getResource <- function(x, name){
   name <- gsub("\\.\\.", "/",name)
-  file <- .getPotentialNames(paths=x@paths, pattern=name, name=name)
+  file <- grep(name,x@paths, value=TRUE)
   ## append full URL
   file <- paste(x@curPath, file, sep="/")
   ## Assuming that we have only got one item...
@@ -61,6 +48,7 @@
     objName <- load(file=url(file))
     return(get(objName))
   }else{
+    message("Available results: ")
     return(file) ## TODO: why all 4 when I should only have two?
   }
 }
@@ -77,7 +65,6 @@ setMethod("$", "AnnotationHub",
 
 
 ## TODO:
-## 1) figure out the bug where we list all the results if nothing matches...
 ## 2) make code that downloads differently for when things are not .rda files?
 
 
