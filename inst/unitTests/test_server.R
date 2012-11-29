d6740aeab30d0140e74687d779ed8862b8ea6a35
Marc Carlson <mcarlson@fhcrc.org>
2012-11-29 00:43:49 +0000

    Adds framework for new kinds of tests.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@71539 bc3139a8-67e5-0310-9ffc-ced21a209358

---
+++ inst/unitTests/test_server.R
@@ -0,0 +1,14 @@
+## a set of methods to make sure that our server is live and the that
+## current URLs are all valid.
+
+## 1st get the string for the base server
+server <- .getServer()
+
+
+test_server <- function(){
+    ## Now I need to basically ping that string (plus other things) to
+    ## check various things...
+    
+
+    
+}
