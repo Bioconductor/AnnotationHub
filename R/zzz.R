33512d210a6eec2d4c85508403f96536c1760b86
Marc Carlson <mcarlson@fhcrc.org>
2013-02-07 19:34:30 +0000

    Makes caching automatic and move the default location to "~"
    For windows this will be in C:\Users\username\AppData\.AnnotationHub
    and for mac and windows it will be in ~\.AnnotationHub
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@73240 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/zzz.R
+++ R/zzz.R
@@ -1,15 +1,20 @@
-## onload will check if you have used caching before and if not print
-## a helpful message
+## onload will set your caching to your home dir by default.
 
 .onLoad <- function(libname, pkgname)
 {
     if (is.na(hubCache())) {
-        cache <- tempfile()
-        dir.create(tempfile(), recursive=TRUE)
+        os <- .Platform$OS.type
+        basePath <- switch(os,
+                           "unix" = path.expand("~"),
+                           "windows"= file.path(dirname(path.expand("~")),
+                             "AppData"))
+        cache <- file.path(basePath, ".AnnotationHub")
+        suppressWarnings(dir.create(cache, recursive=TRUE))
         options(AnnotationHub.Cache=cache)
-        message("See ?hubCache to enable a permanent AnnotationHub cache")
     }
 }
 
+
 ## Load cpp Trie module
 loadModule("TrieModule", TRUE)
+
