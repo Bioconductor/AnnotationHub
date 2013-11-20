180ac1ffb1b6f14821fd8ab6a115ef970ed3a97f
Dan Tenenbaum <dtenenba@fhcrc.org>
2013-11-20 20:39:04 +0000

    To download files that are not already cached, retrieve the file directly from S3.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@83468 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/hubUtils.R
+++ R/hubUtils.R
@@ -233,9 +233,14 @@ setMethod("possibleDates", "missing", function(x, ...) {
 }
 
 .hubResource <- function(hubUrl, path=character()) {
-    file <- paste(hubUrl, .snapshotVersion(), "resources", sep="/")
+    bucketname <- getOption("ANNOTATION_HUB_BUCKET_NAME", "annotationhub")
+    s3url <- getOption("ANNOTATION_HUB_S3_URL",
+        paste0("http://", bucketname,".s3.amazonaws.com/"))
+    file <- s3url
+    path <- gsub("//", "/", path, fixed=TRUE)
+    path <- sub("^\\/", "", path)
     if (length(path))
-        file <- paste(file, path, sep="/")
+        file <- paste0(file, path)
     file
 }
 
