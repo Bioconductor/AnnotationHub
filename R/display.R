ae23ac62ed14a8b635063677d92a803d6a6dcb05
Marc Carlson <mcarlson@fhcrc.org>
2013-12-13 21:14:53 +0000

    speed ups the method for display()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@84529 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/display.R
+++ R/display.R
@@ -5,12 +5,12 @@ setMethod("display", signature(object="AnnotationHub"),
     md <- metadata(object)
     d <- display(as.data.frame(md)) # cast as dataframe to display in browser
 
-    f <- apply(d,2, unique) 
+    ## f <- apply(d,2, unique) 
 
     # drop the element named "Tags"
-    f2 <- f[-which(names(f) %in% c("Tags"))]
+    ## f2 <- f[-which(names(f) %in% c("Tags"))]
 
-    filters(object) <- f2
+    filters(object) <- as.list(d['RDataPath'])
 
     return(object)
 })
