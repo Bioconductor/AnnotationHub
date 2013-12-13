1d40c8828b0846c17d9a2e37d212a2b511d8524a
Nathaniel Hayden <nhayden@fhcrc.org>
2013-12-13 01:07:38 +0000

    Made test vars more descriptive; added example for display()
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@84476 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_display.R
+++ inst/unitTests/test_display.R
@@ -2,19 +2,18 @@ library(RUnit)
 library(AnnotationHub)
 
 test_display <- function() {
-    message("Most excellent")
+    cat("Most excellent")
     #making a data.frame
-    measrs <- data.frame(gender = c("M", "M",
+    measures <- data.frame(gender = c("M", "M",
     "F"), ht = c(172, 186.5, 165), Tags = c(91,
     99, 74))
 
-
     #check if apply works
-    foo <- apply(measrs,2,unique)
-    checkEquals(foo$gender, c("M","F"))
+    uniqMeasures <- apply(measures,2,unique)
+    checkEquals(uniqMeasures$gender, c("M","F"))
 
     #check if dropping a column works
-    foo2 <- foo[-which(names(foo) %in% c("Tags"))]
-    checkEquals(names(foo2), c("gender","ht"))
+    noTags <- uniqMeasures[-which(names(uniqMeasures) %in% c("Tags"))]
+    checkEquals(names(noTags), c("gender","ht"))
     #checkEquals(all(names(foo2) %in% c("gender","ht")))
 }
