2268edb53a0d16918b7aecb723741f4dc5e57d6c
Sonali Arora <sarora@fhcrc.org>
2014-01-10 17:50:29 +0000

    1) passing parameters in display.R for adding more design functionality to display() for an AnnotationHub object 2) Adding a screen shot for the vignette 3) editing the AnnotationHub.Rnw for adding the description about display() for an AnnotationHub object
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@85409 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/display.R
+++ R/display.R
@@ -1,11 +1,35 @@
 setMethod("display", signature(object="AnnotationHub"),
     function(object, ...) 
-{
-    md <- metadata(object)
+{ 
+    md <- metadata(ah)
     drops <-c("Title", "TaxonomyId")
     md <- md[,!(names(md) %in% drops)]
-    #Concatenate a string with show(object) and pass to the next line..
-    d <- display(as.data.frame(md)) 
+    summaryMessage = capture.output(show(ah))
+    serverOptions= list(
+        bSortClasses=TRUE,
+        aLengthMenu = c(1000, 5000, "All"),
+        iDisplayLength = 1000,
+        "sDom" = '<"top"i>rt<"top"f>lt<"bottom"p><"clear">')
+    ch <- list( 
+            '<script type="text/javascript"
+                     class="shiny-html-output"
+                     language="javascript" 
+                     src="/js/editable.min.js" ></script>', 
+            '<script class="jsbin" 
+                     src="/js/jquery.dataTables.nightly.js" ></script>', 
+            '<script class="shiny-html-output" 
+                     type="text/javascript" 
+                     src="/js/demo.js" ></script>',
+            '<script src = "/js/shiny2.js" ></script>' )
+    d <- display(object =as.data.frame(md), summaryMessage = summaryMessage, 
+        serverOptions = serverOptions, style=ch )
     filters(object) <- as.list(d['RDataPath'])
     return(object)
 })
+
+# ch <- c(
+# 'tags$script(type="text/javascript",class="shiny-html-output", language="javascript", src="/js/editable.min.js")',  
+# 'tags$script(class="jsbin", class="shiny-html-output", src="/js/jquery.dataTables.nightly.js")',  
+# 'tags$script(class="shiny-html-output", src="/js/demo.js")', 
+# 'tags$script(class="shiny-html-output", src = "/js/shiny2.js")') #NEW 
+
