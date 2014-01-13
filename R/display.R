6141f011e3f0e4230ef622321d2b624c48a6c513
Sonali Arora <sarora@fhcrc.org>
2014-01-13 22:58:56 +0000

    Removed java script files and  added functionality in R for a) truncating columns b) formatting the Tags to display spaces c) adding a unique identifier column to filter the AnnotationHub object.
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@85492 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/display.R
+++ R/display.R
@@ -1,35 +1,37 @@
 setMethod("display", signature(object="AnnotationHub"),
     function(object, ...) 
 { 
-    md <- metadata(ah)
+    #metadata- do not touch
+    md <- metadata(object)
+    md[["Tags"]] <- unstrsplit(md[["Tags"]], sep=", ")
+        
+    #create a data frame copy for edits called df
+    df <- as.data.frame(md)
+    
+    #Adding a unique identifier
+    Id <- seq_len(nrow(df))
+    df <- cbind(Id, df)
+    
+    #dropping column names
     drops <-c("Title", "TaxonomyId")
-    md <- md[,!(names(md) %in% drops)]
-    summaryMessage = capture.output(show(ah))
+    df <- df[,!(names(df) %in% drops), drop=FALSE]
+    
+    #Truncating extra large columns
+    df[["Tags"]] <- substr(df[["Tags"]], 1, 10)
+    df[["RDataPath"]] <- substr(df[["RDataPath"]], 1, 10)
+    
+    summaryMessage = capture.output(show(object))
     serverOptions= list(
         bSortClasses=TRUE,
         aLengthMenu = c(1000, 5000, "All"),
         iDisplayLength = 1000,
         "sDom" = '<"top"i>rt<"top"f>lt<"bottom"p><"clear">')
-    ch <- list( 
-            '<script type="text/javascript"
-                     class="shiny-html-output"
-                     language="javascript" 
-                     src="/js/editable.min.js" ></script>', 
-            '<script class="jsbin" 
-                     src="/js/jquery.dataTables.nightly.js" ></script>', 
-            '<script class="shiny-html-output" 
-                     type="text/javascript" 
-                     src="/js/demo.js" ></script>',
-            '<script src = "/js/shiny2.js" ></script>' )
-    d <- display(object =as.data.frame(md), summaryMessage = summaryMessage, 
-        serverOptions = serverOptions, style=ch )
-    filters(object) <- as.list(d['RDataPath'])
-    return(object)
+        
+        
+    d <- display(object =df, summaryMessage = summaryMessage, 
+                 serverOptions = serverOptions)
+    filterString <- md[gsub(" ","",as.vector(d[,1])),"RDataPath"]
+    filters(object) <- list(RDataPath = filterString)
+    object
 })
-
-# ch <- c(
-# 'tags$script(type="text/javascript",class="shiny-html-output", language="javascript", src="/js/editable.min.js")',  
-# 'tags$script(class="jsbin", class="shiny-html-output", src="/js/jquery.dataTables.nightly.js")',  
-# 'tags$script(class="shiny-html-output", src="/js/demo.js")', 
-# 'tags$script(class="shiny-html-output", src = "/js/shiny2.js")') #NEW 
 
