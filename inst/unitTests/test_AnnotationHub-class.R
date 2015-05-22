27aa54c86b66096bcdef1a56aa35bd1ec4f78ff2
Sonali Arora <sarora@fhcrc.org>
2015-05-22 23:35:54 +0000

    unit Test update
    the query test was testing if 11 records were returned( one for each ensembl 
    release) Since release 80 gtf files were added to the hub, this was breaking
     the unitTest as there are now 12 records!
    So have updated the unitTest to not rely on the number of ensembl versions
     
    
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@104130 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_AnnotationHub-class.R
+++ inst/unitTests/test_AnnotationHub-class.R
@@ -1,7 +1,7 @@
 test_query <- function() {
     ah = AnnotationHub()
     q1 <- query(ah, c("GTF", "Ensembl", "Homo sapiens"))
-    checkEquals(11, nrow(mcols(q1)))
+    checkTrue("AH7558" %in% names(q1))
     nm <- c("title", "dataprovider", "species", "taxonomyid", "genome", 
         "description", "tags", "rdataclass", "sourceurl", "sourcetype")
     checkEquals(nm, names(mcols(q1)))
