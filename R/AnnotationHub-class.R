a71c463634e4a4c1254a815a07e085506ab3cc73
Martin Morgan <mtmorgan@fhcrc.org>
2015-08-14 20:36:12 +0000

    tweak error message about record availability
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@107444 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/AnnotationHub-class.R
+++ R/AnnotationHub-class.R
@@ -446,8 +446,9 @@ setMethod("c", "AnnotationHub",
 
     className <- sprintf("%sResource", .dataclass(x))
     if (is.null(getClassDef(className))) {
-        msg <- sprintf("'%s' not available in this version of AnnotationHub;
-                        use biocLite('AnnotationHub') to update?", names(x))
+        msg <- sprintf("'%s' not available in this version of the
+            AnnotationHub package; use biocLite('AnnotationHub') to update?",
+            names(x))
         stop(paste(strwrap(msg, exdent=2), collapse="\n"), call.=FALSE)
     }
 
