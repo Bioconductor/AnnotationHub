d3c0d512c1ff64b08e0056a478def158568f423c
Valerie Obenchain <vobencha@fhcrc.org>
2016-11-15 21:22:51 +0000

    edits to .get1,GenomicScores-method
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@124028 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GenomicScoresResource-class.R
+++ R/GenomicScoresResource-class.R
@@ -6,16 +6,13 @@ setMethod(".get1", "GenomicScoresResource",
     .require("GenomicScores")
     path2rds <- cache(getHub(x))
     object <- readRDS(path2rds)
-
     mdobj <- metadata(object)
-    gscores <- GScores(provider=mdobj$provider,
-                       provider_version=mdobj$provider_version,
-                       download_url=mdobj$download_url,
-                       download_date=mdobj$download_date,
-                       reference_genome=mdobj$reference_genome,
-                       data_pkgname=mdobj$data_pkgname,
-                       data_dirpath=dirname(path2rds)) ## is 'dirname()'
-
     rm(object)
-    gscores
+    GScores(provider=mdobj$provider,
+            provider_version=mdobj$provider_version,
+            download_url=mdobj$download_url,
+            download_date=mdobj$download_date,
+            reference_genome=mdobj$reference_genome,
+            data_pkgname=mdobj$data_pkgname,
+            data_dirpath=dirname(path2rds))
 })
