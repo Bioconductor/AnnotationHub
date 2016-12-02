1526eafccd24b82b4d03cd4ddb86bf75b3ca2fa9
Valerie Obenchain <vobencha@fhcrc.org>
2016-12-02 18:58:02 +0000

    modify Robert's .get1,GenomicScoresResource-method
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/trunk/madman/Rpacks/AnnotationHub@124750 bc3139a8-67e5-0310-9ffc-ced21a209358

--- R/GenomicScoresResource-class.R
+++ R/GenomicScoresResource-class.R
@@ -7,12 +7,15 @@ setMethod(".get1", "GenomicScoresResource",
     path2rds <- cache(getHub(x))
     object <- readRDS(path2rds)
     mdobj <- metadata(object)
-    rm(object)
-    GScores(provider=mdobj$provider,
-            provider_version=mdobj$provider_version,
-            download_url=mdobj$download_url,
-            download_date=mdobj$download_date,
-            reference_genome=mdobj$reference_genome,
-            data_pkgname=mdobj$data_pkgname,
-            data_dirpath=dirname(path2rds))
+    gsco <- GScores(provider=mdobj$provider
+                    provider_version=mdobj$provider_version
+                    download_url=mdobj$download_url,
+                    download_date=mdobj$download_date,
+                    reference_genome=mdobj$reference_genome,
+                    data_pkgname=mdobj$data_pkgname,
+                    data_dirpath=dirname(path2rds))
+    scorlelist <- get(gsco@data_pkgname, envir=gsco@.data_cache)
+    scorlelist[[mdobj$seqname]] <- object
+    assign(mdobj$data_pkgname, scorlelist, envir=gsco@.data_cache)
+    gsco
 })
