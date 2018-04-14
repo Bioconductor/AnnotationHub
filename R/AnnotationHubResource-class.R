6650256666c5aef5221cb5814222baa564efc213
Martin Morgan <martin.morgan@roswellpark.org>
2018-04-14 10:59:14 -0400

    undo 109305f (invalid genome for gtf file)
    - updated in rtracklayer f2650c6

--- R/AnnotationHubResource-class.R
+++ R/AnnotationHubResource-class.R
@@ -134,12 +134,7 @@ setMethod(".get1", "GTFFileResource",
     .require("rtracklayer")
     .require("GenomeInfoDb")
     yy <- getHub(x)
-    gtf <- try({
-        rtracklayer::import(cache(yy), format="gtf", genome=yy$genome, ...)
-    }, error = function(e) {
-        message("importing gtf without specifying seqinfo")
-        rtracklayer::import(cache(yy), format="gtf", ...)
-    })
+    gtf <- rtracklayer::import(cache(yy), format="gtf", genome=yy$genome, ...)
     .tidyGRanges(x, gtf)
 })
 
