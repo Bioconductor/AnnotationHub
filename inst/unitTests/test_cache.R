d12417402ec1720315686e45d9215e3ccdbfd507
lshep <lori.shepherd@roswellpark.org>
2019-02-20 11:56:18 -0500

    Fix AnnotationHub vignette and tests for updated Rsamtools
      - No longer accepts razip files
      - Updated vignette to use gff and twobit
      - removed test for Id with multiple files as there are no currently active in AnnotationHub

--- inst/unitTests/test_cache.R
+++ inst/unitTests/test_cache.R
@@ -7,10 +7,13 @@ test_cache_datapathIds <- function() {
     checkIdentical(result, structure(34294L, .Names = "AH28854"))
 
     ## 1:several mapping
-    result <- AnnotationHub:::.datapathIds(hub["AH169"])
-    checkIdentical(result,
-                   structure(c(169L, 14130L), .Names = c("AH169", "AH169")))
+    #result <- AnnotationHub:::.datapathIds(hub["AH169"])
+    #checkIdentical(result,
+    #               structure(c(169L, 14130L), .Names = c("AH169", "AH169")))
+    # Removed old ra zip files that needed index
+    #   Currently no id associated with two files in 3.9
 
+    
     ## unknown identifier
     result <- AnnotationHub:::.datapathIds(hub["AH0"])
     checkIdentical(result, setNames(integer(), character()))
